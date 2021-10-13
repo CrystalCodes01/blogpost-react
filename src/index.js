b304e08a2c75eee4787c7d288c65818165d3126b
CrystalCodes01 <crystalcodes.0@gmail.com>
2021-10-13 12:40:02 -0400

    children props

--- src/index.js
+++ src/index.js
@@ -2,28 +2,43 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Jane"
-        time="Yesterday at 11am"
-        comment="Great!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Bryan"
-        time="Today at 6pm"
-        comment="Nice Post!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Crystal"
-        time="Today at 11pm"
-        comment="Thanks!"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          <p>Are you reviewing posts?</p>
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          time="Yesterday at 11am"
+          comment="Great!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Bryan"
+          time="Today at 6pm"
+          comment="Nice Post!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Crystal"
+          time="Today at 11pm"
+          comment="Thanks!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
